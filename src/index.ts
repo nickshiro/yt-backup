2d91591395f0d7d20c616d6ffe068bd6abb5a2a4
skewb1k <skewb1kunix@gmail.com>
2025-09-29 14:06:28 +0300

    Init grammy tg bot

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import { Bot } from "grammy";
+
+const botToken = process.env.YTB_BOT_TOKEN;
+if (!botToken) {
+	throw new Error("Environment variable YTB_BOT_TOKEN is not defined");
+}
+
+const bot = new Bot(botToken);
+
+bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+
+bot.on("message", (ctx) => ctx.reply("Got another message!"));
+
+bot.start();
